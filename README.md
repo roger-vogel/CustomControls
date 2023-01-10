664afde87b07f071cff5f5cd7e4122f9f95eaec0
Roger Vogel <roger@puldypartners.com>
2023-01-09 20:17:39 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CustomControls
+
+A description of this package.
---
+++ Sources/CustomControls/CustomControls.swift
@@ -0,0 +1,6 @@
+public struct CustomControls {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
