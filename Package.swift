dd525e14a6a5b0976bcfb6104b67369e42807fa1
Roger Vogel <roger@puldypartners.com>
2023-01-09 23:20:20 -0800

    initial Commit

--- Package.swift
+++ Package.swift
@@ -7,20 +7,18 @@ let package = Package(
     name: "CustomControls",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CustomControls",
-            targets: ["CustomControls"]),
-    ],
+        .library( name: "CustomControls",targets: ["CustomControls"]),],
+  
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+        .package(url: "https://github.com/roger-vogel/ColorManager.git", from: "1.0.0"),.package(url: "https://github.com/roger-vogel/Extensions.git", from: "1.0.0"),],
+
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CustomControls",
-            dependencies: []),
+            dependencies: [.product(name: "ColorManager", package: "ColorManager"),.product(name: "Extensions", package: "Extensions")]),
         .testTarget(
             name: "CustomControlsTests",
             dependencies: ["CustomControls"]),
--- Tests/CustomControlsTests/CustomControlsTests.swift
+++ Tests/CustomControlsTests/CustomControlsTests.swift
@@ -6,6 +6,6 @@ final class CustomControlsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CustomControls().text, "Hello, World!")
+        //XCTAssertEqual(CustomControls().text, "Hello, World!")
     }
 }
