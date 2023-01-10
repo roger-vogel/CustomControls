d5826b68be9cf2eccbd6f5d8057ae367567a998c
Roger Vogel <roger@puldypartners.com>
2023-01-09 23:23:39 -0800

    Created separate file for bordered button

---
+++ Sources/CustomControls/RoundedBorderButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoundedBorderButton.swift
+//  
+//
+//  Created by Roger Vogel on 1/9/23.
+//
+
+import UIKit
+import ColorManager
+import Extensions
+
+class RoundedBorderButton: UIButton {
+
+    // MARK: -  INITIALIZATION
+    override func awakeFromNib() {
+        
+        self.setBorder(width: 1.0, color: UIColor.darkGray.cgColor)
+        roundAllCorners(value: 10)
+      
+        super.awakeFromNib()
+    }
+    
+    public func setBorderColor(color: ColorManager) {
+        
+        self.setBorder(width: 1.0, color: color.cgcolor)
+    }
+
+}
